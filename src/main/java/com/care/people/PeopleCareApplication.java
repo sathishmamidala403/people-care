ba83acc2228703ce29ac08876de151c9c7b62ce0
Mamidala <smamidala@galaxe.com>
2025-09-03 11:49:36 +0530

    initial commit

---
+++ src/main/java/com/care/people/PeopleCareApplication.java
@@ -0,0 +1,13 @@
+package com.care.people;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleCareApplication.class, args);
+	}
+
+}
